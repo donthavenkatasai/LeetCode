be62f292cc2c877d90d950627f3fb5f4ad71a899
donthavenkatasai <donthavenkatasai18@gmail.com>
2026-05-28 09:31:54 +0530

    Added Toeplitz Matrix Problem

---
+++ ArraysProblems/ToeplitzMatrix(766).cpp
@@ -0,0 +1,46 @@
+/*766. Toeplitz Matrix
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m =matrix.size()-1;
+        int n = matrix[0].size()-1;
+        for(int i = 0;i <m; i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] != matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
